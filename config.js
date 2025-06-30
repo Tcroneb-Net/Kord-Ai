743cb55bd38bb63c3dce459da1f072c1ff67b73e
LIL CRONE <tcronebtech2015@gmail.com>
2025-06-30 04:28:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'kord_ai-FBSKEDNgupwddXDr',
     OWNER_NUMBER: process.env.OWNER_NUMBER || "263788533181",
     WORKTYPE: process.env.WORKTYPE || "public",
-    PREFIX: process.env.PREFIX || "[.]",
+    PREFIX: process.env.PREFIX || ".",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || true,
     MONGODB_URI: process.env.MONGODB_URI || "",
     STATUS_VIEW: process.env.STATUS_VIEW || true,
