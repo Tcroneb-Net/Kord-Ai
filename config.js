7e22871e52c3139f6c4e7c0c013686344536d8be
LIL CRONE <tcronebtech2015@gmail.com>
2025-07-08 18:39:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || '',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2349067339193",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "263788533181",
     WORKTYPE: process.env.WORKTYPE || "private",
     PREFIX: process.env.PREFIX || "[.]",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || true,
     MONGODB_URI: process.env.MONGODB_URI || "",
     STATUS_VIEW: process.env.STATUS_VIEW || true,
     SAVE_STATUS: process.env.SAVE_STATUS || false,
-    LIKE_STATUS: process.env.SAVE_STATUS || false,
-    STATUS_EMOJI: process.env.STATUS_EMOJI || "🥏",
+    LIKE_STATUS: process.env.SAVE_STATUS || true,
+    STATUS_EMOJI: process.env.STATUS_EMOJI || "💚",
     ERROR_MSG: process.env.ERROR_MSG || true,
     TIMEZONE: process.env.TIMEZONE || "Africa/Lagos",
     CAPTION: process.env.CAPTION || "",
